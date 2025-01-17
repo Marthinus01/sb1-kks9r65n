2e9e0ce3467ad5a8e26ed07229fd80199b8305dd
Marthinus01 <116526990+Marthinus01@users.noreply.github.com>
2025-01-17 20:15:53 +0200

    Update locations.ts

--- src/data/locations.ts
+++ src/data/locations.ts
@@ -4,7 +4,7 @@ export const locations: Location[] = [
   {
     name: 'Eloff-Delmas',
     address: 'Eloff-Delmas Road, Mpumalanga',
-    contacts: ['012-345-6789', '012-345-6790', '083-294-3391'],
+    contacts: '068 616 1100',
     isUpcoming: false,
     coordinates: { lat: -26.0757, lng: 28.9506 }
   },
@@ -23,23 +23,23 @@ export const locations: Location[] = [
     coordinates: { lat: -24.7007, lng: 28.4163 }
   },
   {
-    name: 'Alberton',
+    name: 'Kliprevier-Alberton',
     address: 'Kliprevier Road, Next to Mall of the South, Gauteng',
-    contacts: ['015-345-6789', '015-345-6790', '015-345-6791'],
+    contacts: '068 616 1100',
     isUpcoming: true,
     coordinates: { lat: -26.2708, lng: 28.1224 }
   },
   {
     name: 'Mookgophong',
     address: 'Mookgophong Central, Limpopo',
-    contacts: ['016-345-6789', '016-345-6790', '016-345-6791'],
+    contacts: '082 335 6245',
     isUpcoming: true,
     coordinates: { lat: -24.5308, lng: 28.7316 }
   },
   {
     name: 'Brakpan',
     address: 'Brakpan Central, Gauteng',
-    contacts: ['017-345-6789', '017-345-6790', '017-345-6791'],
+    contacts: '068 616 1100',
     isUpcoming: true,
     coordinates: { lat: -26.2359, lng: 28.3697 }
   }
